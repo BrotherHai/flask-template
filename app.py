eb92a2986703145ce5c4bac0a56077a8bb53737f
BrotherHai <50613992+BrotherHai@users.noreply.github.com>
2023-10-22 21:34:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
-import os
+import feedparser
+from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 @app.route('/')
-def index():
-    print("GET /")
-    return "Welcome, this is a Flask app deployed on Zeabur"
+def home():
+    name=request.args["name"]
+    page_dict = feedparser.parse(name)
+    return page_dict
+
+@app.route('/about')
+def about():
+    return 'About'
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
